12113cd5a4fc6ee57de7a7bc829e5b92c28db527
LiuShulong <bigfish_liu@126.com>
2015-04-05 22:29:52 +0800

    初始化

---
+++ SLSetFont/SLSetFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLSetFont
+//
+//  Created by LiuShulong on 4/5/15.
+//  Copyright (c) 2015 LiuShulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SLSetFont/SLSetFont/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLSetFont
+//
+//  Created by LiuShulong on 4/5/15.
+//  Copyright (c) 2015 LiuShulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
